3de690b2a38e7323e007681e3187d98c4a08cd17
DuvanJ <87352830+DuvanJ@users.noreply.github.com>
2021-07-24 17:29:00 +1200

    Cambios

--- src/Domino/Tren.java
+++ src/Domino/Tren.java
@@ -9,6 +9,50 @@ package Domino;
  *
  * @author chuca
  */
-public class Tren {
+public class Tren 
+{
+    private String id;
+    private int capacidad;
     
+
+    public Tren(String id, int capacidad) {
+        this.id = id;
+        this.capacidad = capacidad;
+        
+    }
+
+    public Tren() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    
+    
+     @Override
+    public String toString()
+    {
+        String info="***INFORMACION DEL TREN***";
+         
+        info+="\nID: "+this.getId()
+            +"\nCapacidad"+this.getCapacidad();
+        return info;
+    }
+    
+    Tren tren1=new Tren ("1",50);
+    Tren tren2=new Tren ("2",50);
+    Tren tren3=new Tren ("3",50);
 }
